d7430f3dfb5e402acaaf1d7712c764a8adcb2450
koomahnah <koomahnah@gmail.com>
2014-09-16 06:31:29 +0000

    better rebuild

--- hello_rebuild_reload.sh
+++ hello_rebuild_reload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make -C ~/linux-raspberrypi/ M=`pwd` modules && sudo ./hello_unload.sh && sudo ./hello_load.sh
+make -C ~/linux/ M=`pwd` modules && sudo ./hello_unload.sh; sudo ./hello_load.sh
